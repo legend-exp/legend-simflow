574496029fac85f0e5b64a463c51a3befb483965
tdixon <tdixon@berkeley.edu>
2026-01-30 05:22:06 -0800

    allow no data

--- workflow/src/legendsimflow/scripts/init-julia-env.jl
+++ workflow/src/legendsimflow/scripts/init-julia-env.jl
@@ -14,11 +14,13 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import Pkg
 
+
 if !ispath(joinpath(first(DEPOT_PATH), "registries", "LegendJuliaRegistry"))
     @info("Installing Legend Julia package registry")
     Pkg.Registry.add("General")
     Pkg.Registry.add(url = "https://github.com/legend-exp/LegendJuliaRegistry.git")
 end
+
 
 Pkg.instantiate()
 Pkg.precompile()
