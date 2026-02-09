7138577a416017d90c341461c633a7b6a8fc7224
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-02-09 14:29:07 +0100

    plot t_maxA as drift time and other plotting improvements

--- workflow/src/legendsimflow/scripts/plots/tier_hit_observables.py
+++ workflow/src/legendsimflow/scripts/plots/tier_hit_observables.py
@@ -25,6 +25,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 from legendsimflow import metadata as mutils
 from legendsimflow import nersc, plot
+from legendsimflow.plot import n_nans
 
 args = nersc.dvs_ro_snakemake(snakemake)  # noqa: F821
 
@@ -34,7 +35,7 @@ simid = args.wildcards.simid
 
 
 def fig(table):
-    fig = plt.figure(figsize=(12, 6))
+    fig = plt.figure(figsize=(14, 8))
 
     data = plot.read_concat_wempty(hit_files, table)
 
@@ -78,15 +79,18 @@ def fig(table):
 
     # A/E
     ax = fig.add_subplot(gs_bot[0, 0])
-    h_aoe = hist.new.Reg(200, 0, 1.2, name="A/E").Double()
+    h_aoe = hist.new.Reg(200, 0, 2, name="A/E").Double()
     h_aoe.fill(data.aoe)
-    plot.plot_hist(h_aoe, ax, color="tab:red")
+
+    plot.plot_hist(h_aoe, ax, color="tab:red", n_nans=n_nans(data.aoe))
 
     # drift time
     ax = fig.add_subplot(gs_bot[0, 1])
-    h_dt = hist.new.Reg(300, 0, 3000, name="drift time (ns)").Double()
-    h_dt.fill_flattened(data.drift_time)
-    plot.plot_hist(h_dt, ax, color="tab:orange")
+    h_dt = hist.new.Reg(
+        300, 0, 3000, name="drift time · $t_{max(A)} - t_0$ (ns)"
+    ).Double()
+    h_dt.fill_flattened(data.drift_time_amax)
+    plot.plot_hist(h_dt, ax, color="tab:orange", n_nans=n_nans(data.drift_time_amax))
 
     # usability
     ax = fig.add_subplot(gs_bot[0, 2])
