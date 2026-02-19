9d9b4c7f3da3a5dec94cd6c9f1a28faffdd4874d
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-02-19 23:22:44 +0100

    evt: switch to a much faster implementation of read_data_at_channel_as_ak()
    ak.flatten() + indexing is somehow much faster than just indexing the
    jagged array

--- workflow/src/legendsimflow/profile.py
+++ workflow/src/legendsimflow/profile.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 import contextlib
 import logging
 import time
-from collections import OrderedDict, defaultdict
+from collections import defaultdict
 from collections.abc import Callable
 
 import psutil
@@ -36,7 +36,6 @@ def _pct(x: int | float) -> str:
 
 def make_profiler() -> tuple[Callable, Callable]:
     proc = psutil.Process()
-    stats = OrderedDict()
     stats = defaultdict(
         lambda: {
             "wall_s": 0.0,
