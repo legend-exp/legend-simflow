61412ff680ef519309a24c341f761275f9908404
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2026-09-16 21:27:01 +0100

    small fix

--- workflow/src/LegendSimflow.jl/src/pulse_shape_sim_helpers.jl
+++ workflow/src/LegendSimflow.jl/src/pulse_shape_sim_helpers.jl
@@ -520,7 +520,7 @@ function setup_hpge_simulation(meta_path::String,
     end
     @info "Simulated depletion voltage is $dep"
 
-    if !(vdep !== nothing && abs(vdep * u"V" - dep) > threshold * u"V"
+    if !(vdep !== nothing && abs(vdep * u"V" - dep) > threshold * u"V")
         error("Difference between measured and simulated depletion is larger than $threshold V!")
     end
 
